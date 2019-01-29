eaa953f60bc320e7752fa3dc3d620957c7039d7a
Tristan Bennett <betriistan@gmail.com>
2019-01-28 18:37:48 -0600

    inserted favorites into database

--- insert-favorites.sql
+++ insert-favorites.sql
@@ -1,3 +1,18 @@
-INSERT INTO Favorite_Songs()
+INSERT INTO Favorite_Songs(user_Key, song_Key)
 VALUES
-	()
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(1, 5),
+	(1, 6),
+	(1, 7),
+	(1, 8),
+	(1, 9),
+	(1, 10),
+	(1, 11),
+	(1, 12),
+	(2, 8),
+	(3, 7),
+	(4, 8),
+	(5, 3)
