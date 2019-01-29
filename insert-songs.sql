598d517fbdfbf13bff8e81407914ae7cbc7df7f6
Tristan Bennett <betriistan@gmail.com>
2019-01-28 18:29:03 -0600

    inserted songs to database

--- insert-songs.sql
+++ insert-songs.sql
@@ -1,3 +1,14 @@
 INSERT INTO SONGS(song_ID, song_artist, song_name)
 VALUES
-	()
+	(1, 1, 'Pharoahs & Pyramids'),
+	(2, 1, 'Living Upside Down'),
+	(3, 2, 'Ritual Union'),
+	(4, 2, 'Killing Me'),
+	(5, 3, 'What''s It Gonna Take'),
+	(6, 3, 'Still Think'),
+	(7, 4, 'Your Teeth In My Neck'),
+	(8, 4, 'After The Storm (feat. Tyler, The Creator & Bootsy Collins)'),
+	(9, 5, 'Montreal'),
+	(10, 5, 'Night Moves'),
+	(11, 6, 'Slumlord'),
+	(12, 6, 'Dear Skorpio Magazine')
